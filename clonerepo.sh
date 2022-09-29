6e99f89b4a6a8d9dc92704dcca44bd49993b73e6
unknown <enzo2000enzo.h@gmail.com>
2022-09-29 12:19:24 +0200

    refactor: adding comment

--- clonerepo.sh
+++ clonerepo.sh
@@ -1,8 +1,12 @@
-echo "Comment voulez vous nommer le dossier ou sera cloner le repository :"
+#Demande à lutilisateur de saisir le nom du dossier pour cloner le repository dans celui-ci
+echo "Comment voulez vous nommer le dossier où sera cloner le repository :"
 read localFolder 
 echo " "
+
+#Chemin du repository
 repository="https://github.com/KeligMartin/4-SRC.git"
 
+#Clone avec valeur de la variable repository vers la valeur de la variable localFolder
 git clone "$repository" "$localFolder"
 echo " "
-echo "Le repository $repository à bien été clonné dans $localFolder"
+echo "Le repository $repository a bien été cloné dans $localFolder"
